2c9259e4d32682070e57d9dd4195db5014eaa468
David Hervas <bioestadistica@iislafe.es>
2019-10-14 00:31:52 +0200

    Fixed bug in new version of fix.dates when handling NA values

--- R/descriptive.R
+++ R/descriptive.R
@@ -563,8 +563,8 @@ fxd <- function(d, locale="C", use.probs=TRUE){
   final_dates <- do.call("c", lapply(1:length(d), function(y) na.omit(do.call("c", lapply(co, function(x) x[y])))[1]))
   years <- as.numeric(substr(final_dates, 1, 4))
   median_year <- median(years)
-  final_dates[abs(years - median_year) > abs(years-100 - median_year)] <- do.call(c, lapply(final_dates[abs(years - median_year) > abs(years-100 - median_year)], function(x) seq(x, length=2, by="-100 years")[2]))
-  final_dates[abs(years - median_year) > abs(years+100 - median_year)] <- do.call(c, lapply(final_dates[abs(years - median_year) > abs(years+100 - median_year)], function(x) seq(x, length=2, by="100 years")[2]))
+  final_dates[abs(years - median_year) > abs(years-100 - median_year)] <- do.call(c, lapply(final_dates[abs(years - median_year) > abs(years-100 - median_year)], function(x) tryCatch(seq(x, length=2, by="-100 years")[2], error=function(e) NA)))
+  final_dates[abs(years - median_year) > abs(years+100 - median_year)] <- do.call(c, lapply(final_dates[abs(years - median_year) > abs(years+100 - median_year)], function(x) tryCatch(seq(x, length=2, by="100 years")[2], error=function(e) NA)))
   return(final_dates)
   Sys.setlocale("LC_TIME", "")
 }
