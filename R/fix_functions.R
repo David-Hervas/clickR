b643b13fa7b3c7c2bd90d293943f3ddd343fd336
David Hervas <daherma@eio.upv.es>
2021-03-11 23:23:38 +0100

    Improved fix.levels functionality

--- R/fix_functions.R
+++ R/fix_functions.R
@@ -330,7 +330,8 @@ fix.levels <- function(data, factor_name, method="dl", levels=NULL, plot=FALSE, 
   groups <- cutree(clusters, k=k)
   if (!is.null(levels)){
     if(length(levels) == 1 && levels == "auto") levels <- sapply(split(names(groups), groups), function(x) names(sort(table(x), decreasing=TRUE))[1])
-    output <- factor(groups, levels=1:k, labels=levels[apply(sapply(split(as.data.frame(stringdist::stringdistmatrix(tolower(x_na), tolower(levels), method=method, useNames = TRUE)), groups), colMeans), 2, which.min)])
+    assig_lvls <- apply(sapply(split(as.data.frame(stringdist::stringdistmatrix(tolower(x_na), tolower(levels), method=method, useNames = TRUE)), groups), colMeans), 1, which.min)
+    output <- factor(groups, levels=assig_lvls, labels=names(assig_lvls))
   } else{
     output <- groups
     cat("Groups of levels:\n \n")
