e1cc9f88efd7ea5d955a251b2a85412885f825cc
David Hervas <ddhervas@yahoo.es>
2023-07-31 16:45:25 +0200

    Added messy version of mtcars dataset

---
+++ R/messy_mtcars-data.R
@@ -0,0 +1,22 @@
+#' Messy Motor Trend Car Road Tests Dataset
+#'
+#' Modified version of the mtcars dataset with different types of errors
+#' in the data. The dataset has 13 variables and 32 observations.
+#'
+#' @docType data
+#'
+#' @usage data(messy_mtcars)
+#'
+#' @format A data frame with 32 observations and 13 variables
+#'
+#' @keywords datasets
+#'
+#' @references Henderson and Velleman (1981), Building multiple regression
+#' models interactively. Biometrics, 37, 391–411.
+#'
+#' @source \code{datasets} package
+#'
+#' @examples
+#' data(messy_mtcars)
+#' descriptive(messy_mtcars)
+"messy_mtcars"
